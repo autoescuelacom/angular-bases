dd44aa7d26351251f7ce617516bd030ef0c73367
Daida Gonzalez <daidadev@outlook.com>
2025-02-04 18:24:20 +0100

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { CounterModule } from './counter/counter.module';
+import { DbzModule } from './dbz/dbz.module';
 import { HeroesModule } from './heroes/heroes.module';
 
 
@@ -15,8 +16,9 @@ import { HeroesModule } from './heroes/heroes.module';
     BrowserModule,
     CounterModule,
     HeroesModule,
-    AppRoutingModule
-],
+    AppRoutingModule,
+    DbzModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
